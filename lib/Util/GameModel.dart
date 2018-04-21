13455c5c3e96fd118d7602f641e9cae275550875
FilipeA <filipera1995@hotmail.com>
2018-04-21 12:54:31 +0100

    ups forget something

--- lib/Util/GameModel.dart
+++ lib/Util/GameModel.dart
@@ -3,10 +3,9 @@ import 'package:haterz/Util/QuestionModel.dart';
 import 'package:haterz/Util/UserModel.dart';
 
 class Game{
-
+  String id;
   String name;
   User user1, user2;
   Question questions;
   Message messages;
-
 }
