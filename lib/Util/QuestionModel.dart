61696af1804ee0de580000c0be953acb8fa443d1
FilipeA <filipera1995@hotmail.com>
2018-04-21 12:48:39 +0100

    Updated: QuestionModel

--- lib/Util/QuestionModel.dart
+++ lib/Util/QuestionModel.dart
@@ -2,6 +2,7 @@ class Question{
 
   String id;
   String question;
-  
-  String answer_1, answer_2;
+  String hate1, hate2; // this is where the options are store
+  String answer_user1_photo, answer_user2_photo; //we store the image of the user here
+
 }
